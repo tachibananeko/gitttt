31debf375a683e2375050a380b05ad0cccc4d1a5
cheenlam <redsinsue@gmail.com>
2020-12-13 15:01:23 +0800

    no message

---
+++ mission06/script/common.js
@@ -0,0 +1,13 @@
+$('#index_back').click(function(){location.href="index.html"})
+
+function showBox(text){
+    $('#alert_mask').css('display','')
+    $('#alert_mask .alert_box').text(text);
+    setTimeout(function(){
+        $('#alert_mask').css('display','none')
+    },2000)
+}
+
+$('#menuBar').click(function(){$('#ph_menu').toggleClass('on')})
+$(window).resize(function(){ $('#ph_menu').removeClass('on')})
+
